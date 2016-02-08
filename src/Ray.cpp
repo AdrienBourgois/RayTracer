6a618e266feedb88df558de6099878da2cb411b8
Antonio Cantarella <a.cantarella@student.isartdigital.com>
2016-02-08 16:14:38 +0100

    |Ray.cpp| : Created Ray class. Still WIP.

---
+++ src/Ray.cpp
@@ -0,0 +1,29 @@
+#include "Ray.h"
+#include "RenderBuffer.h"
+Ray::Ray(Vector3D<float> position, Vector2D<float> screen_size, float idx_x, float idx_y, RenderBuffer* rend_buff)
+{
+    this->render_buffer = rend_buff;
+    this->start_point = position;
+    
+    this->findDestPoint(screen_size, idx_x, idx_y);
+
+    this->direction = this->dest_point - start_point;
+
+    this->run();
+}
+
+auto Ray::findDestPoint(Vector2D<float> screen_size, float idx_x, float idx_y) -> void
+{
+
+    float NDC_x = (idx_x +0.5f) / screen_size.x;
+    float NDC_y = (idx_y +0.5f) / screen_size.y;
+
+    this->dest_point.x = ((2.f * NDC_x) - 1.f) * (screen_size.x / screen_size.y);
+    this->dest_point.y = 1.f - (2.f * NDC_y);
+    
+}
+
+auto Ray::run() -> void
+{
+    
+}
