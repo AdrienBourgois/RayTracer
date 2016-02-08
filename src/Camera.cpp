faac914eac6a7c7dc40c2e86ffbcdbb4881d9581
Antonio Cantarella <a.cantarella@student.isartdigital.com>
2016-02-08 16:03:59 +0100

    |Camera.cpp| :

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -1,28 +1,37 @@
 #include "Camera.h"
+#include "Ray.h"
 #include <iostream>
 
 Camera::Camera()
 {
     this->FOV = 0;
     this->lookAt = Vector3D<float> (0.f, 0.f, 0.f);
+    this->ray = nullptr;
 }
 
-auto Camera::init(Vector2D<int> screen_res) -> void
+auto Camera::init(Vector2D<int> screen_res, RenderBuffer* rend_buffer) -> void
 {
+    this->render_buffer = rend_buffer;
     std::cout<<"LookAt = "<<this->lookAt<<std::endl;
 
 	this->position = Vector3D<float>(0.f, 0.f, -1.0f);
-    
     this->viewDirection = this->lookAt - this->position;
-
     this->up = Vector3D<float> (0.f, 1.f, 0.f);
-    this->screen_size = screen_res;
+    this->screen_size.x = float (screen_res.x);
+    this->screen_size.y = float (screen_res.y);
     this->u = viewDirection * up;
     this->v = u * viewDirection;
-    std::cout<<"Pos = "<< this->position<<std::endl;
-    std::cout<<"ViewDirection = "<< this->viewDirection<<std::endl;
-    std::cout<<"LookAt after - = "<< this->lookAt<<std::endl;
-    std::cout<<"Up = "<< this->up<<std::endl;
-    std::cout<<"U = "<< this->u<<std::endl;
-    std::cout<<"V = "<< this->v<<std::endl;
+}
+
+auto Camera::render() -> void
+{
+    if (this->ray == nullptr)
+    {
+        this->ray = new Ray(this->position, this->screen_size, 1.f /* idx_x*/, 1.f /*idx_y*/, this->render_buffer);
+    }
+}
+
+auto Camera::shot() -> void
+{
+
 }
