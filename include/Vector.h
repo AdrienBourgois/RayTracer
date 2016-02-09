78e519bc3b74f4babdeddd182c1c82bf4b8ff0fd
Antonio Cantarella <a.cantarella@student.isartdigital.com>
2016-02-09 10:47:04 +0100

    |Vector.h| :

--- include/Vector.h
+++ include/Vector.h
@@ -48,7 +48,7 @@ class Vector3D
         Vector3D&   operator +=(Vector3D const& a); 
         Vector3D&   operator -=(Vector3D const& a);
         Vector3D    operator *(Vector3D const& a);
-        Vector3D    operator *(Vector3D const& a);
+        Vector3D    operator *(float const& a);
         bool        operator ==(Vector3D const& a); 
 
     private:
