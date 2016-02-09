82570f7b430fdfebdfaa0530fe20d91785708353
Mathias Rigaut <m.rigaut@student.isartdigital.com>
2016-02-09 12:24:38 +0100

    |Vector.h| :

--- include/Vector.h
+++ include/Vector.h
@@ -42,6 +42,7 @@ class Vector3D
         Vector3D&   operator =	(Vector3D const& a);
         Vector3D   	operator +	(Vector3D const& a); 
         Vector3D   	operator -	(Vector3D const& a); 
+		float		operator -	(double a);
         Vector3D&   operator +=	(Vector3D const& a); 
         Vector3D&   operator -=	(Vector3D const& a);
         Vector3D	operator *	(Vector3D const& a);
