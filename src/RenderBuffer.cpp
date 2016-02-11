53205b10c9a42d5abfd0b687e309b2190055da8f
Antonio Cantarella <a.cantarella@student.isartdigital.com>
2016-02-11 15:58:17 +0100

    Cleaning the code

--- src/RenderBuffer.cpp
+++ src/RenderBuffer.cpp
@@ -1,3 +1,28 @@
 #include "RenderBuffer.h"
+#include "Log.h"
 
+RenderBuffer::RenderBuffer()
+{
+    Log* log = Log::getInstance();
+    log->info("RenderBuffer created.");
+}
 
+auto RenderBuffer::setColorList(Vector3D<float> col) -> void
+{
+    this->color_list.push_back(col);
+}
+
+auto RenderBuffer::setScreenCoordList(Vector2D<float> screen_coord) -> void
+{
+    this->screen_coord_list.push_back(screen_coord);
+}
+
+auto RenderBuffer::getColorList() -> std::vector<Vector3D<float>>
+{
+    return this->color_list;
+}
+
+auto RenderBuffer::getScreenCoordList() -> std::vector<Vector2D<float>>
+{
+    return this->screen_coord_list;
+}
