2832060476e48520cbd0c29bbb383ca9eb35b7de
Antonio Cantarella <a.cantarella@student.isartdigital.com>
2016-02-18 10:47:38 +0100

    Specular doesnt work anymore... Try to fixing it

--- src/Ray.cpp
+++ src/Ray.cpp
@@ -246,17 +246,19 @@ auto Ray::calculateDiffuseLight(SceneNode* node, SceneNode* light) -> Vector3D<U
 auto Ray::calculateSpecularLight(SceneNode* node, SceneNode* light) -> Vector3D<Uint8>
 {
 	Vector3D<float> n = (this->calculateNormal(node)).normalize();
-	Vector3D<float> l = (this->collision_point - light->getPosition()).normalize();
+	//Vector3D<float> l = (this->collision_point - light->getPosition()).normalize();
+	Vector3D<float> l = (light->getPosition() - this->collision_point).normalize();
 	Vector3D<float> v = this->direction.normalize();
 
 	//Vector3D<float> r = (n - l);// * (2 * DOT(n, l));
 
 	//r = r * (2 * DOT(n, l));
 	float nl = DOT(n, l);
-	Vector3D<float> r = (n * nl * n) - l;
+	Vector3D<float> r = (n * nl * 2.f) - l;
 	r = r.normalize();
  
-	Vector3D<float> specular_light = /*konstant_light(from 0 to 1)*/ (Vector3D<float> (255.f, 255.f, 255.f) * 0.70f) * float(DOT(r, v.pow(2.f)));
+	//Vector3D<float> specular_light = /*konstant_light(from 0 to 1)*/ (Vector3D<float> (255.f, 255.f, 255.f) * 0.30f) * float(DOT(r, v.pow(2.f)));
+	Vector3D<float> specular_light = /*konstant_light(from 0 to 1)*/ (Vector3D<float> (255.f, 255.f, 255.f) * 0.30f) * float (pow(DOT(r, v) ,2.f));
 	
 	//std::cout<<"specular_light = "<<specular_light<<std::endl;	
 
