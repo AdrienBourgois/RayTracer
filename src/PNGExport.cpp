a0ac87aad689e3f63c706e99e0d9db617fd0b777
Adrien Bourgois <a.bourgois@student.isartdigital.com>
2016-02-25 15:27:19 +0100

    |PNGExport.cpp| : Add new parameters in constructor and open file in constructor

--- src/PNGExport.cpp
+++ src/PNGExport.cpp
@@ -1,5 +1,7 @@
 #include "PNGExport.h"
 
-PNGExport::PNGExport(void* _pointer, std::string _pathFile)
-: pointer(_pointer), pathFile(_pathFile)
-{}
+PNGExport::PNGExport(void* _data, int _sizePerData, int _width, int _height, std::string _pathFile)
+: inData(_data), pathFile(_pathFile), width(_width), height(_height), sizePerData(_sizePerData)
+{
+    this->file.open(this->pathFile);
+}
