038dd755aba2e22dfbe5c3af6d01a5c3bc98ede9
Nicolas Bocquet <n.bocquet@student.isartdigital.com>
2016-02-26 09:45:32 +0100

    begining merging code of v1 and v2

--- src/CollisionCalc.cpp
+++ src/CollisionCalc.cpp
@@ -5,6 +5,16 @@
 #include "Vector.h"
 #include "Log.h"
 
+auto calculateCollision(GeometryBuffer* current_geometry, Ray* ray) -> bool
+{
+        Log* log = Log::getInstance();
+
+        if(current_geometry == nullptr)
+                log->error("Can't calculate collision. Reason: current_geometry is null.");
+
+	
+}
+
 auto calculateCollision(SphereGeometryBuffer* current_geometry, Ray* ray) -> bool
 {
 	Log* log = Log::getInstance();
