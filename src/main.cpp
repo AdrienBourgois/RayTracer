d02f5e78abc7636968957d1af72593d6d01dc6d3
Adrien Bourgois <a.bourgois@student.isartdigital.com>
2016-02-26 16:18:39 +0100

    |main.cpp| : Remove image after display

--- src/main.cpp
+++ src/main.cpp
@@ -42,8 +42,10 @@ auto main() -> int
             ImGui::SetNextWindowPos(ImVec2(30, 30), ImGuiSetCond_FirstUseEver);
             ImGui::Begin("Image", &show_another_window, ImGuiWindowFlags_NoSavedSettings);
             ImGui::Text("Test");
-            gui.displayImage(raytracer->getRenderBuffer()->getFormattedData<Uint8>(false), screen_res.x, screen_res.y);
+            Uint8* image = raytracer->getRenderBuffer()->getFormattedData<Uint8>(false);
+            gui.displayImage(image, screen_res.x, screen_res.y);
             ImGui::End();
+            delete image;
         }
         gui.render();
 		raytracer->getRenderBuffer()->clearBuffer();
