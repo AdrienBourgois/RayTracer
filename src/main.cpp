9718921b77dc5623831f4e1d3eef9f7369eeb437
Mathias Rigaut <m.rigaut@student.isartdigital.com>
2016-02-26 13:24:58 +0100

    Modification of the main

--- src/main.cpp
+++ src/main.cpp
@@ -24,8 +24,14 @@ auto main() -> int
 	SceneNode* scene_node = new SceneNode();
 	scene_node->init(Vector3D<float> (0.f, 0.f, -2.f), Vector3D<float> (255.f, 255.f, 0.f), false, 1.f);
 
+	SceneNode* sun_node = new SceneNode();
+	sun_node->init(Vector3D<float> (0.f, 2.f, -2.f), Vector3D<float> (255.f, 255.f, 255.f), true, 1.f);
+
 	raytracer->genGeometryBuffer(scene_node->getPosition(), scene_node->getRadius(), scene_node->getVerticeList(), EGeometry_type::SPHERE);
 	raytracer->genMaterialBuffer(scene_node->getColor(), 100.f, 0.f, false);
+
+	raytracer->genGeometryBuffer(sun_node->getPosition(), sun_node->getRadius(), sun_node->getVerticeList(), EGeometry_type::SPHERE);
+	raytracer->genMaterialBuffer(sun_node->getColor(), 100.f, 0.f, true);
 
     //SDL_Surface* image = IMG_Load("assets/RinL.jpg");
     //unsigned char pixels[] = {255, 255, 255, 255, 0, 0};
